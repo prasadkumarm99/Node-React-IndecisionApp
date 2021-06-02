7fbb33506612c6fc596701acca11f96688bdf554
Prasad Kumar <prasadkumarmedicherla@gmail.com>
2021-06-02 09:13:39 +0530

    Variable data and control statements

--- Learning/public/scripts/anonyApp.js
+++ Learning/public/scripts/anonyApp.js
@@ -1,35 +1,81 @@
 "use strict";
 
-var list = ["Name", "Email", "Phone"];
+// Control statements in JSX
 
-var template = React.createElement(
-    "div",
+var details1 = {
+  name: "Raju",
+  email: "example@gmail.com",
+  phone: 9876543210
+};
+
+var details2 = {
+  name: null,
+  email: null,
+  phone: 9876543210
+};
+var namer = function namer(details) {
+  if (details.name) {
+    return details.name;
+  } else {
+    return "Unknown";
+  }
+};
+
+var template1 = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
     null,
-    React.createElement(
-        "h1",
-        null,
-        "KnowIT Learnings Data"
-    ),
-    React.createElement(
-        "ol",
-        null,
-        React.createElement(
-            "li",
-            null,
-            list[0]
-        ),
-        React.createElement(
-            "li",
-            null,
-            list[1]
-        ),
-        React.createElement(
-            "li",
-            null,
-            list[2]
-        )
-    )
+    "KnowIT Learnings Details Template 1"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Name: ",
+    namer(details1)
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Email: ",
+    details1.email ? details1.email : "Unknown"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Phone: ",
+    details1.phone
+  )
 );
 
-var root = document.getElementById("root");
-ReactDOM.render(template, root);
+var template2 = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "KnowIT Learnings Details Template 2"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Name: ",
+    namer(details2)
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Email: ",
+    details2.email ? details2.email : "Unknown"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Phone: ",
+    details2.phone
+  )
+);
+
+ReactDOM.render(template1, document.getElementById("root"));
+ReactDOM.render(template2, document.getElementById("sub"));
