a9fc3281aff144e0162a88f67f375867633ad063
Prasad Kumar <prasadkumarmedicherla@gmail.com>
2021-06-12 17:59:34 +0530

    port change

--- Learning/webpack.config.js
+++ Learning/webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
   },
   devtool: 'eval-source-map',
   devServer: {
-    contentBase: path.join(__dirname, 'public')
+    contentBase: path.join(__dirname, 'public'),
+    port: 9000
   }
 }
